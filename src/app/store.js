6aef4694cf9af0f178387fa96d90f6243dde96bb
Oluwatosin Gbenga <tosinolugbenga@gmail.com>
2023-07-15 22:01:00 +0100

    redux done

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,9 @@
-import { configureStore } from '@reduxjs/toolkit';
-import counterReducer from '../features/counter/counterSlice';
+import { configureStore } from "@reduxjs/toolkit";
+import boolReducer from '../Slices/Bool/boolSlice'
+
 
 export const store = configureStore({
-  reducer: {
-    counter: counterReducer,
-  },
-});
+    reducer: {
+        bool: boolReducer
+    }
+})
